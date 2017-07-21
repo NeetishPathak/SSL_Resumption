06befd1360cb52fb2a2b4f2aba6dbc5fab6b1648
PATHAK <niits23@gmail.com>
2017-07-21 14:53:10 -0700

    update test

--- network/common.h
+++ network/common.h
@@ -86,7 +86,7 @@
 #define PSK_BDATA "0533c95c9ecc310ee07cb70a316c45448487c1f70bbea99fe6616f3348305677"
 
 /*Early Data*/
-#define EARLY_DATA TRUE
+#define EARLY_DATA FALSE
 #define READ_WRITE_TEST TRUE
 //#define WRITE_DATA "GET / HTTP/1.1\r\nHost: \r\nConnection: close\r\n\r\n"
 #define WRITE_DATA "Message \0"
